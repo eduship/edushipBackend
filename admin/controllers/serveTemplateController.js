7457176bfff266bf2930d5ed41a3863bee4331e8
hacker <fynn.weyrich@web.de>
2018-10-19 18:05:11 +0200

    neue AdminArea

--- admin/controllers/serveTemplateController.js
+++ admin/controllers/serveTemplateController.js
@@ -1,11 +1,21 @@
+os = require('os-utils');
+
 exports.get_dashboard = function(req, res, next){
     if (req.user){
-        res.render('dashboard');
+        os.cpuUsage(function(v){
+            console.log(Math.round(os.freememPercentage()*100))
+            res.render('dashboard', {
+                freemem : Math.round(os.freememPercentage()*100),
+                usedcpu : Math.round(v*100),
+                processUptime : os.processUptime()
+            });
+        });
     } else {
         res.redirect('/login');
     }
 }
 
+//if req.user render admin page else return to login
 exports.get_login = function(req, res, next){
     if(req.user){
         res.redirect('/');
